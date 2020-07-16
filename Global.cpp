b599e07c0a0b231f3ef87af8a25ce24cb2b90e1b
Karol Baraniecki <karol@baraniecki.eu>
2020-07-16 18:43:39 +0200

    Refactor Global::get_variable to be a bit more readable

--- Global.cpp
+++ Global.cpp
@@ -11,11 +11,13 @@ std::optional<std::string> Global::get_variable(const std::string &name)
     }
 
     auto search = g.variables.find(name);
-    if(search == g.variables.end()) {
-        char *from_env = getenv(name.c_str());
-        if(!from_env)
-            return {};
-        return { from_env };
+    if(search != g.variables.end()) {
+        return { search->second };
     }
-    return { search->second };
+
+    char *from_env = getenv(name.c_str());
+    if(from_env)
+        return { from_env };
+
+    return {};
 }
